c4d9cd3f6623f88b1664cc655a9cba73a5a7bb76
bruhmann <ruhmann@gmail.com>
2017-08-06 11:59:29 -0500

    updated description
    to include the Malyan M200 which is what the MP Select Mini is a rebrand of

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Makes it possible to change the EEPROM values of Monoprice Select Mini 3D Firmware through OctoPrint based on OctoPrint-EEprom-Marlin by amsbr"""
+plugin_description = """Makes it possible to change the EEPROM values of Monoprice Select Mini 3D and Malyan M200 Firmware through OctoPrint based on OctoPrint-EEprom-Marlin by amsbr"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Brian Ruhmann"
